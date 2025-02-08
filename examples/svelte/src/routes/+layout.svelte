18661362bbcf65b0dcc6795cfe33688b63419750
wighawag <wighawag@gmail.com>
2025-02-08 16:35:04 +0000

    basic service-worker

--- examples/svelte/src/routes/+layout.svelte
+++ examples/svelte/src/routes/+layout.svelte
@@ -1,6 +1,9 @@
 <script lang="ts">
+	import VersionAndInstallNotfications from '$lib/web/service-worker/VersionAndInstallNotfications.svelte';
 	import '../app.css';
 	let { children } = $props();
 </script>
 
 {@render children()}
+
+<VersionAndInstallNotfications src="" alt="" />
---
+++ examples/svelte/src/routes/+layout.ts
@@ -0,0 +1,4 @@
+import '$lib/web/service-worker/service-worker-handler';
+
+export const prerender = true;
+export const trailingSlash = 'always';
