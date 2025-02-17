8aecbab4abf772281f95927fb39a5abc80e61c99
wighawag <wighawag@gmail.com>
2025-02-17 11:29:35 +0000

    private-account done

--- examples/svelte/src/lib/private-account/index.ts
+++ examples/svelte/src/lib/private-account/index.ts
@@ -1,4 +1,5 @@
 import { derived, type Readable } from 'svelte/store';
+import { privateKeyToAddress } from 'viem/accounts';
 
 // TODO ensure it is the best type
 type OwnerAccount = { address: string | undefined } | undefined;
@@ -21,6 +22,10 @@ export type PrivateAccount =
 			signer: Signer;
 	  };
 
+function computeStorageKey(ownerAddress: string): string {
+	return `__private_account_${ownerAddress.toLowerCase()}`;
+}
+
 export function createPrivateAccount<Data>(params: {
 	ownerAccount: Readable<OwnerAccount> & { getLocalSigner(): Promise<Signer> };
 	skipConfirmation?: boolean;
@@ -28,11 +33,47 @@ export function createPrivateAccount<Data>(params: {
 	let _state: PrivateAccount = undefined;
 	let _set: ((value: PrivateAccount) => void) | undefined;
 	let _ownerAccount: OwnerAccount | undefined;
+	let _cache: Map<string, Signer> = new Map();
 
 	function setState(newState: PrivateAccount) {
 		_state = newState;
 		if (_set) {
 			_set(newState);
+		}
+	}
+
+	function getSavedSigner(ownerAddress: string) {
+		let signer: Signer | undefined = undefined;
+		const storageKey = computeStorageKey(ownerAddress);
+		const fromMemory = _cache.get(ownerAddress.toLowerCase());
+		if (fromMemory) {
+			signer = fromMemory;
+		} else {
+			let fromStorage: string | null | undefined;
+			try {
+				fromStorage = localStorage.getItem(storageKey);
+			} catch {}
+			if (fromStorage && fromStorage.startsWith('0x')) {
+				const privateKey = fromStorage as `0x${string}`;
+				signer = {
+					privateKey,
+					address: privateKeyToAddress(privateKey)
+				};
+			}
+		}
+		return signer;
+	}
+
+	function saveSigner(ownerAddress: string, signer: Signer, options?: { toStorage?: boolean }) {
+		_cache.set(ownerAddress.toLowerCase(), signer);
+		if (options?.toStorage) {
+			const storageKey = computeStorageKey(ownerAddress);
+			try {
+				localStorage.setItem(storageKey, signer.privateKey);
+			} catch (err) {
+				// TODO error
+				console.error(err);
+			}
 		}
 	}
 
@@ -45,7 +86,10 @@ export function createPrivateAccount<Data>(params: {
 			const newOwner = $ownerAccount.address;
 			if (_ownerAccount?.address !== newOwner) {
 				_ownerAccount = $ownerAccount;
-				if (params.skipConfirmation) {
+				const savedSigner = getSavedSigner(newOwner);
+				if (savedSigner) {
+					setState({ owner: newOwner, step: 'Settled', signer: savedSigner });
+				} else if (params.skipConfirmation) {
 					_requestSigner(newOwner);
 				} else {
 					setState({ owner: newOwner, step: 'RequireConfirmation', signer: undefined });
@@ -60,6 +104,7 @@ export function createPrivateAccount<Data>(params: {
 		setState({ owner: newOwner, step: 'Generating', signer: undefined });
 		const value = await getSigner();
 		if (newOwner === _ownerAccount?.address) {
+			saveSigner(newOwner, value);
 			setState({ owner: _ownerAccount.address, step: 'Settled', signer: value });
 		} else {
 			// setState will have been called then, so we just ignore the result of getSigner
